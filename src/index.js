5625de1641f870357833f49695673079dfd2c4a9
Tim Bedford <tmbdfd@gmail.com>
2020-12-09 16:08:58 -0500

    Don't recreate drag image for every track's event handler

--- src/index.js
+++ src/index.js
@@ -22,11 +22,10 @@ tracks.forEach((track, idx) => {
 trackBank.appendChild(ul);
 
 document.querySelectorAll(".track-listing").forEach(el => {
+  const blank_record = new Image();
+  blank_record.src = "./src/images/record_sleeve.png";
   el.addEventListener('dragstart', e => {
     e.dataTransfer.setData("text/plain", el.id);
-    const blank_record = new Image();
-    blank_record.src = "./src/images/record_sleeve.png";
-    blank_record.classList.add("drag-record");
     e.dataTransfer.setDragImage(blank_record, 150, 150);
   })
 });
